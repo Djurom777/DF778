a9974966ddab31ff9d1276857daaea8473c7b277
DJUROM777 <djurom777@ya.ru>
2025-08-06 00:20:04 +0300

    Initial Commit

---
+++ DF778/DF778App.swift
@@ -0,0 +1,17 @@
+//
+//  DF778App.swift
+//  DF778
+//
+//  Created by IGOR on 06/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct DF778App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DF778Tests/DF778Tests.swift
@@ -0,0 +1,16 @@
+//
+//  DF778Tests.swift
+//  DF778Tests
+//
+//  Created by IGOR on 06/08/2025.
+//
+
+import Testing
+
+struct DF778Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
